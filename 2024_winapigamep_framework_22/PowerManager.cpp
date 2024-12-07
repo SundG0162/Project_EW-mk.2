fe3b903a2dddadfd3bf02ee152a61e6eea39be57
SundG0162 <123521471+SundG0162@users.noreply.github.com>
2024-12-07 20:17:16 +0900

    feat : ResultScene

--- 2024_winapigamep_framework_22/PowerManager.cpp
+++ 2024_winapigamep_framework_22/PowerManager.cpp
@@ -1,9 +1,14 @@
 #include "pch.h"
 #include "PowerManager.h"
+#include "ResultManager.h"
 
 void PowerManager::modifyPower(int value)
 {
 	int prev = _power;
+	if (value > 0)
+	{
+		GET_SINGLETON(ResultManager)->modifyTotalPower(value);
+	}
 	_power += value;
 	OnPowerChangeEvent.invoke(prev, _power);
 }
